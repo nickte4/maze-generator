1ed1849465819c0bf9da1192c86251394a9b6bc8
nickte4 <ntopacio25@gmail.com>
2023-04-11 23:44:58 -0400

    Add walls to cell.js
    Added variable this.walls to Cell class which tracks
    whether a wall exists on the top, right, bottom, and/or left.
    Used lines to draw these walls on the screen.

--- cell.js
+++ cell.js
@@ -3,6 +3,8 @@ class Cell {
   constructor(i, j) {
     this.i = i;
     this.j = j;
+    // [top, right, bottom, left]
+    this.walls = [true, true, true, true];
   }
   show(gridWidth) {
     stroke(255);
@@ -10,14 +12,14 @@ class Cell {
     let x = this.i * w;
     let y = this.j * w;
 
-    // line from top left to top right
-    line(x, y, x + w, y);
-    // line from top right to bottom right
-    line(x + w, y, x + w, y + w);
-    // line from bottom right to bottom left
-    line(x + w, y + w, x, y + w);
-    // line from bottom left to top left
-    line(x, y + w, x, y);
+    // top wall
+    if (this.walls[0]) line(x, y, x + w, y);
+    // right wall
+    if (this.walls[1]) line(x + w, y, x + w, y + w);
+    // bottom wall
+    if (this.walls[2]) line(x + w, y + w, x, y + w);
+    // left wall
+    if (this.walls[3]) line(x, y + w, x, y);
 
     // noFill();
     // rect(this.i * gridWidth, this.j * gridWidth, gridWidth - 1, gridWidth - 1);
